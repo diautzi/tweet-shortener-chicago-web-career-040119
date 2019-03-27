74e379f10fbbe858f0615cf7d732bb8439f52281
Diana Utan <dia_utzi@yahoo.com>
2019-03-27 04:44:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,38 +1,51 @@
 # Write your code here.
 
-def word_substituter(tweet)
-  words = tweet.split(" ")
-  short_tweet = []
+# def word_substituter(tweet)
+#   words = tweet.split(" ")
+#   short_tweet = []
   
-    words.each do |word|
-     case word.downcase 
-     when "hello"
-       short_tweet << "hi"
-     when "to", "two", "too" 
-        short_tweet << "2"
-     when "for", "four" 
-      short_tweet << "4"
-     when "be" 
-      short_tweet << "b"
-     when "you"
-       short_tweet << "u"
-     when "at"
-        short_tweet << "@"
-     when "and"
-        short_tweet << "&"
-     else
-        short_tweet << word 
+#     words.each do |word|
+#     case word.downcase 
+#     when "hello"
+#       short_tweet << "hi"
+#     when "to", "two", "too" 
+#         short_tweet << "2"
+#     when "for", "four" 
+#       short_tweet << "4"
+#     when "be" 
+#       short_tweet << "b"
+#     when "you"
+#       short_tweet << "u"
+#     when "at"
+#         short_tweet << "@"
+#     when "and"
+#         short_tweet << "&"
+#     else
+#         short_tweet << word 
+#     end
+#   end
+#     short_tweet.join(" ")
+#   end
+             
+# def bulk_tweet_shortener(tweets)
+#   tweets.each do |tweet|
+#     tweet = word_substituter(tweet)
+#     puts "#{tweet}"
+#   end 
+# end
+ 
+ def word_substituter(tweet)
+  array = tweet.split(" ")
+  array.each_with_index do |word, index|
+    dictionary.each do |key, value|
+      if word == key
+        array[index] = value
+      end
     end
   end
-    short_tweet.join(" ")
-  end
-             
- def bulk_tweet_shortener(tweets)
-   tweets.each do |tweet|
-     tweet = word_substituter(tweet)
-     puts "#{tweet}"
-   end 
- end
+   array.join(" ")
+end
+
  
  def selective_tweet_shortener(tweets)
      if tweets.length > 140
